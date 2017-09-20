d3cb90b59bb6580d88434db978c16a2d46b7e971
pasaunders <kaorti@gmail.com>
2017-09-19 19:21:51 -0700

    expanded book models

--- user_login/book_authors/models.py
+++ user_login/book_authors/models.py
@@ -3,6 +3,17 @@ from django.db import models
 
 class Books(models.Model):
     """Books in database."""
+
+    liked_users = models.ManyToManyField(
+        'users',
+        related_name='liked_books',
+        null=True,
+    )
+    uploader = models.ForeignKey(
+        'users',
+        related_name='uploaded_books',
+        null=True,
+    )
     name = models.CharField(max_length=60)
     desc = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now=True)
@@ -18,5 +29,14 @@ class Authors(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class users(models.Model):
+    """Users on book site."""
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.CharField(max_length=60)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
